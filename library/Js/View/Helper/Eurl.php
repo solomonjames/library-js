cf07349140755033f18c382817482ac5c6721f30
James Solomon <solomonjames@gmail.com>
2012-06-05 22:38:22 -0400

    The download helper was never really finished, and not sure its needed anymore.  Also the Eurl needed some comment fixing

--- library/Js/View/Helper/Eurl.php
+++ library/Js/View/Helper/Eurl.php
@@ -3,21 +3,23 @@
 /**
  * Helper for making easy links and getting urls that depend on the routes and router
  *
- * @package    Zend_View
+ * @package    Js_View
  * @subpackage Helper
- * @copyright  Copyright (c) 2005-2011 Zend Technologies USA Inc. (http://www.zend.com)
- * @license    http://framework.zend.com/license/new-bsd     New BSD License
  */
 class Js_View_Helper_Eurl extends Zend_View_Helper_Abstract
 {
     /**
+     * This stands for "easy url", because it places the route name
+     *      first in the order of parameters.
+     *
      * Generates an url given the name of a route.
      *
-     * @access public
+     * 
      *
-     * @param  mixed $name The name of a Route to use. If null it will use the current Route
+     * @param  mixed $name       The name of a Route to use. If null it will use the current Route
      * @param  array $urlOptions Options passed to the assemble method of the Route object.
-     * @param  bool $reset Whether or not to reset the route defaults with those provided
+     * @param  bool  $reset      Whether or not to reset the route defaults with those provided
+     * @param  bool  $encode
      * @return string Url for the link href attribute.
      */
     public function eurl($name = null, array $urlOptions = array(), $reset = false, $encode = true)
